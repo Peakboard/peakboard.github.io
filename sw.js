b849900451d4494c39b1d8d21849704df2b09c50
Benjamin Donan <benjamin.donan@peakboard.com>
2018-03-26 15:50:16 +0200

    Re Wrote Service Woker

--- sw.js
+++ sw.js
@@ -1,48 +1,29 @@
+var CACHE = 'cache-only';
 
-  self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
+self.addEventListener('install', function(evt){
+    console.log('The service worker is being installed');
 
-  var deferredPrompt;
-
-  window.addEventListener('beforeinstallprompt', function(e) {
-    console.log('beforeinstallprompt Event fired');
-    e.preventDefault();
-
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-
-    return false;
-  });
-
-  btnSave.addEventListener('click', function() {
-    if(deferredPrompt !== undefined) {
-      // The user has had a positive interaction with our app and Chrome
-      // has tried to prompt previously, so let's show the prompt.
-      deferredPrompt.prompt();
-
-      // Follow what the user has done with the prompt.
-      deferredPrompt.userChoice.then(function(choiceResult) {
-
-        console.log(choiceResult.outcome);
-
-        if(choiceResult.outcome == 'dismissed') {
-          console.log('User cancelled home screen install');
-        }
-        else {
-          console.log('User added to home screen');
-        }
-
-        // We no longer need the prompt.  Clear it up.
-        deferredPrompt = null;
-      });
-    }
-  });
+    evt.waitUntil(precache());
 });
+
+self.addEventListener('fetch',function(evt){
+  console.log('The service woker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+});
+
+function precache(){
+  return caches.open(CACHE).then(function(cache){
+    return cache.addAll([
+      './assets'
+    ]);
+  });
+}
+
+
+function fromCache(request){
+  return caches.open(CACHE).then(function(cache)){
+    return cache.match(request).then(fuction(matching{
+      return matching|| Promise.reject('no-match');
+    });
+  });
+}
